bd8a89649e4096a005f295003f596df637ef5fad
chenw <wenchen.mails@gmail.com>
2024-07-04 20:37:46 +0800

    Add src

---
+++ bsp/mini2440/setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# BSP板级代码目录
+export BSP_ROOT="$PWD"
+
+# RT代码路径
+export RTT_ROOT="$PWD/../../"
+
+# 交叉编译工具链路径
+export RTT_EXEC_PATH="/root/Workspace/RT-Thread/gcc-arm-none-eabi-10.3-2021.10/bin"
+
+# 交叉编译工具链前缀
+export RTT_CC_PREFIX="arm-none-eabi-"
